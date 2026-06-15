c6aef20c057b8c9da50359322f2b7904d62a4bdb
PengNi <543943952@qq.com>
2026-06-15 19:43:18 +0800

    fix short read patching and add mapq arg

--- tests/test_args_config.py
+++ tests/test_args_config.py
@@ -47,6 +47,12 @@ class InferenceArgumentAliasesTest(unittest.TestCase):
         self.assertFalse(parser.parse_args([]).gzip)
         self.assertTrue(parser.parse_args(["--gzip"]).gzip)
 
+    def test_inference_mapq_sets_mapq_threshold(self):
+        parser = create_argument_parser("inference")
+
+        self.assertEqual(parser.parse_args([]).mapq_thres, 0)
+        self.assertEqual(parser.parse_args(["--mapq", "20"]).mapq_thres, 20)
+
     def test_inference_help_omits_training_pod5_options(self):
         help_text = create_argument_parser("inference").format_help()
 
